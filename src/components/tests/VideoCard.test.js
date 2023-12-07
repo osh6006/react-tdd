e5cd2fb4983867418d32cabd68f929c8c4d8ead9
OHS <ohs6006@gmail.com>
2023-12-07 11:32:27 +0900

    TEST : Video Card 유닛 테스트

---
+++ src/components/tests/VideoCard.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import useEvent from "@testing-library/user-event";
+import VideoCard from "../VideoCard";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { formatAgo } from "../../util/date";
+import { fakeVideo as video } from "../../tests/videos";
+import { withRouter } from "../../tests/utils";
+
+describe("VideoCard", () => {
+  const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+
+  it("renders video item", () => {
+    render(
+      withRouter(<Route path="/" element={<VideoCard video={video} />} />)
+    );
+
+    const image = screen.getByRole("img");
+
+    expect(image.src).toBe(thumbnails.medium.url);
+    expect(image.alt).toBe(title);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(channelTitle)).toBeInTheDocument();
+    expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video page with video state when clicked", () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      withRouter(
+        <>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </>
+      )
+    );
+
+    const card = screen.getByRole("listitem");
+    useEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
+  });
+});
---
+++ src/tests/videos.js
@@ -0,0 +1,144 @@
+export const fakeVideo = {
+  kind: "youtube#video",
+  etag: "stIFYVpXZI_ujKQ81fQ9rUAwDPI",
+  id: "zwa7NzNBQig",
+  snippet: {
+    publishedAt: "2022-09-23T04:00:19Z",
+    channelId: "UCmlkznD61Olu82oYGFr8gxg",
+    title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+    description:
+      "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/zwa7NzNBQig/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/zwa7NzNBQig/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/zwa7NzNBQig/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "GloRillaVEVO",
+    tags: [
+      "GloRilla",
+      "Cardi",
+      "Tomorrow",
+      "CMG/Interscope",
+      "Records",
+      "Hip",
+      "Hop",
+    ],
+    categoryId: "10",
+    liveBroadcastContent: "none",
+    localized: {
+      title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+      description:
+        "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+    },
+    defaultAudioLanguage: "en",
+  },
+};
+
+export const fakeVideos = [
+  {
+    kind: "youtube#video",
+    etag: "stIFYVpXZI_ujKQ81fQ9rUAwDPI",
+    id: "zwa7NzNBQig1",
+    snippet: {
+      publishedAt: "2022-09-23T04:00:19Z",
+      channelId: "UCmlkznD61Olu82oYGFr8gxg",
+      title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+      description:
+        "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "GloRillaVEVO",
+      tags: [
+        "GloRilla",
+        "Cardi",
+        "Tomorrow",
+        "CMG/Interscope",
+        "Records",
+        "Hip",
+        "Hop",
+      ],
+      categoryId: "10",
+      liveBroadcastContent: "none",
+      localized: {
+        title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+        description:
+          "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+      },
+      defaultAudioLanguage: "en",
+    },
+  },
+  {
+    kind: "youtube#video",
+    etag: "stIFYVpXZI_ujKQ81fQ9rUAwDPI",
+    id: "zwa7NzNBQig2",
+    snippet: {
+      publishedAt: "2022-09-23T04:00:19Z",
+      channelId: "UCmlkznD61Olu82oYGFr8gxg",
+      title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+      description:
+        "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/zwa7NzNBQig/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "GloRillaVEVO",
+      tags: [
+        "GloRilla",
+        "Cardi",
+        "Tomorrow",
+        "CMG/Interscope",
+        "Records",
+        "Hip",
+        "Hop",
+      ],
+      categoryId: "10",
+      liveBroadcastContent: "none",
+      localized: {
+        title: "GloRilla, Cardi B - Tomorrow 2 (Official Music Video)",
+        description:
+          "Stream #Tomorrow2' here: \nhttps://GloRilla.lnk.to/Tomorrow2\n\nFollow #GloRilla:\nInstagram: https://www.instagram.com/glorillapimp/\nTikTok: https://www.tiktok.com/t/ZTR6WV6SC/\nFacebook: https://www.facebook.com/gloyaltygirl\nTwitter: https://twitter.com/GloTheofficial\nFollow #CardiB:\nInstagram: https://www.instagram.com/iamcardib/\nTikTok: https://www.tiktok.com/@iamcardib\nFacebook: https://www.facebook.com/cardib\nTwitter: https://twitter.com/iamcardib/\n\n#Tomorrow2 #GloRilla #CardiB\n\nMusic video by GloRilla, Cardi B performing Tomorrow 2. © 2022 CMG Enterprises/Interscope Records\n\nhttp://vevo.ly/JKU3ao",
+      },
+      defaultAudioLanguage: "en",
+    },
+  },
+];
